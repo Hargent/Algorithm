9622a6f60a1422a7e9a4903f3d653f1b0c0b24ca
Hargent <94790646+Hargent@users.noreply.github.com>
2023-02-12 21:22:51 +0000

    Todays commit 10:22pm 12/02/23

--- permutation.js
+++ permutation.js
@@ -3,18 +3,42 @@
  * @param {number} k
  * @return {string}
  */
-var getPermutation = function (n, k) {
-    let arr = [];
-    const permutate = (n) => {
-        if (n === 0) return;
+// var getPermutation = function (n, k) {
+//     let arr = [];
+//     const permutate = (n) => {
+//         if (n === 0) return;
 
-        console.log(n);
-    };
-    for (let i = 0; i < n; i++) {
-        permutate(i + 1);
+//         console.log(n);
+//     };
+//     for (let i = 0; i < n; i++) {
+//         permutate(i + 1);
+//     }
+//     return arr[k];
+// };
+function permute(num) {
+    num = num.toString();
+    let results = [];
+
+    function backtrack(first = 0) {
+        if (first === num.length) {
+            results.push(num);
+            return;
+        }
+
+        for (let i = first; i < num.length; i++) {
+            // Swap the current element with the first element
+            [num[first], num[i]] = [num[i], num[first]];
+            backtrack(first + 1);
+            // Swap the elements back to their original position
+            [num[first], num[i]] = [num[i], num[first]];
+        }
     }
-    return arr[k];
-};
+
+    backtrack();
+    return results;
+}
+
+console.log(permute(123));
 
 /*
 123
@@ -24,6 +48,6 @@ var getPermutation = function (n, k) {
 312
 321
 */
-console.log(getPermutation(3, 3));
+// console.log(getPermutation(3, 3));
 // console.log(getPermutation(4, 9));
 // console.log(getPermutation(3, 1));
--- sqrt.js
+++ sqrt.js
@@ -1,18 +1,72 @@
-/**
- * @param {number} x
- * @return {number}
- */
-var mySqrt = function (x) {
-    // create start and end
-    let start = 0;
-    let end = x - 1;
-    let arr = Array.from({ length: x }, (end, start) => start + 1);
+// // Yes, the algorithm can be optimized in the following ways:
 
-    // create a list of  numbers between 0 and x
-    // use binary search to check if  squares of the num
-    // if non, run helper funcition again using a diff middle
-    // if it ends up between two num in the list, return the lowest
-    return arr
+// // Instead of creating an array of all integers from 1 to x, you can use binary search to find the square root of x directly.
+
+// // The condition x < arr[middle] ** 2 can be simplified to middle > x / middle.
+
+// // The return value in the final if statement can be simplified to just middle since middle will always be the largest integer that is less than or equal to the square root of x.
+// var mySqrt = function (x) {
+// 	let start = 0;
+// 	let end = x;
+// 	let middle = Math.floor((start + end) / 2);
+
+// 	console.log(middle);
+// 	while (middle > x / middle) {
+// 		end = middle - 1;
+// 		middle = Math.floor((start + end) / 2);
+// 		console.log(middle);
+// 	}
+
+// 	return middle * middle === x ? middle : middle - 1;
+// };
+// // 	let result = x;
+// // 	let precision = 0.00001;
+
+// // const helper = res => {
+// // 	if (Math.abs(x - res * res) > precision) {
+// // 		result = (res + x / res) / 2;
+// // 		helper(result);
+// // 	}
+// // };
+// // helper(result);
+// // while (Math.abs(x - result * result) > precision) {
+// // 	result = (result + x / result) / 2;
+// // }
+// // return Math.floor(result);
+// // };
+// // console.log(mySqrt(4));
+// // console.log(mySqrt(8));
+// console.log(mySqrt(16));
+// // console.log(mySqrt(0));
+// // console.log(mySqrt(1));
+// // console.log(mySqrt(-2));
+// console.log(mySqrt(2147395599));
+
+var mySqrt = function (x) {
+	if (x === 0 || x === 1) return x;
+	if (x < 0) return undefined;
+
+	let start = 0;
+	let end = x;
+	let middle = Math.floor((start + end) / 2);
+
+	while (middle !== x / middle) {
+		if (start > end) return middle;
+		if (middle > x / middle) {
+			end = middle - 1;
+			middle = Math.floor((start + end) / 2);
+		}
+		if (middle < x / middle) {
+			start = middle + 1;
+			middle = Math.floor((start + end) / 2);
+		}
+	}
+
+	return middle;
 };
-console.log(mySqrt(4));
+
+// console.log(mySqrt(16));
+// console.log(mySqrt(1));
 console.log(mySqrt(8));
+// console.log(mySqrt(0));
+// console.log(mySqrt(-4));
